1d2556c8b60c32c3fa4a551a13a420ed537378d4
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-20 23:03:16 +0100

    Create range-for-statement_2.cpp

---
+++ range-for-statement_2.cpp
@@ -0,0 +1,11 @@
+// instead of copying values in v into x, we could set x to refer to an element 
+// & unary suffix = reference to
+// references suitable for specifying function arguments
+
+void increment()
+{
+    int v[] = {0,1,2,3,4,5,6,7,8,9};
+    for (auto& x : v)
+      }}x;
+    // ...
+}
