f49ddc10149ee68256077b4727b2bce76b686f4e
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-20 22:55:51 +0100

    Create range-for-statement.cpp

---
+++ range-for-statement.cpp
@@ -0,0 +1,12 @@
+// set i to zero, while i is not 10, copy the ith element and increment i
+// increment operator ++ adds 1 
+// 1 example of a range-for-statement, a simpler for loop statement for loops that traverse a sequence in a simple way
+
+void print()
+{
+    int v[] = {0,1,2,3,4,5,6,7,8,9};
+
+    for (auto x : v) // for each x in v
+          cout << x << '\n';
+    // ... 
+}
