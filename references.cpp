d515ca1e04bc58b32a5ff4b3bb4683e5508c355a
maxwynne <76392789+maxwynne@users.noreply.github.com>
2023-07-20 23:05:38 +0100

    Create references.cpp

---
+++ references.cpp
@@ -0,0 +1,8 @@
+// making sure my_vec is sorted and no copy is made by using a reference &
+
+void sort(vector<double>& v);
+
+// const = no modifying and don't want the cost of copying
+// example
+
+double sum(const vector<double>&)
